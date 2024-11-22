0b5c88338fd6ef22b0b054ecc60f7ff215df8d95
Rogério Terciotte <rterciotte@gmail.com>
2024-11-22 13:40:20 -0300

    Project created

---
+++ backend/src/test/java/com/rterciotte/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rterciotte.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
