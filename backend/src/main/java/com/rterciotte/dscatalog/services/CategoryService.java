a53b7005ae8b3a6bd299ed74a4dc23304e527c6a
Rogério Terciotte <rterciotte@gmail.com>
2024-11-23 01:31:48 -0300

    DTO

--- backend/src/main/java/com/rterciotte/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rterciotte/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.rterciotte.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rterciotte.dscatalog.dto.CategoryDTO;
 import com.rterciotte.dscatalog.entitites.Category;
 import com.rterciotte.dscatalog.repositories.CategoryRepository;
 
@@ -14,7 +17,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
 }
