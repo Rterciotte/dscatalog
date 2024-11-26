af5ccc7c6e10200ddc9bd30db146256734305034
Rogério Terciotte <rterciotte@gmail.com>
2024-11-25 22:11:55 -0300

    Auditing data

--- backend/src/main/java/com/rterciotte/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/rterciotte/dscatalog/services/CategoryService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
